e4ff1e8f593f7d5a2de39b09db6dceb2df4822bc
Evedelohim <noahpence@gmail.com>
2014-12-31 18:55:59 -0800

    modified:   src/Customer.java 	new file:   src/Engine.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,9 +1,7 @@
-
+import java.util.*; 
 public class Customer {
+	
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
---
+++ src/Engine.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Engine {
+
+	public static void main(String[] args) {
+		
+		Scanner EngInput = new Scanner(System.in); 
+		
+		Customer customerList = new Customer(); 
+		
+		System.out.println("What is your first name? "); 
+		EngInput.nextLine(); 
+		
+
+	
+	
+	}
+}
+
