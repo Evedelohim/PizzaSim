f8b79a6b9ca845a62bbc817f6983da0420180972
Evedelohim <noahpence@gmail.com>
2015-01-02 19:57:18 -0800

    modified:   src/Customer.java 	modified:   src/Engine.java

--- src/Engine.java
+++ src/Engine.java
@@ -1,14 +1,28 @@
-import java.util.Scanner;
+
+import java.util.*; 
 public class Engine {
 
+	
+    public static ArrayList<Customer> CustomerList = new ArrayList<Customer>(); 
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		
 		Scanner EngInput = new Scanner(System.in); 
+		
+		
+		String CusName = ""; 
 		
 		Customer customerList = new Customer(); 
 		
 		System.out.println("What is your first name? "); 
 		EngInput.nextLine(); 
+		
+		
 		
 
 	
